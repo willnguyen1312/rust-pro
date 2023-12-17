c5b3a99a8b228e7d42c441fab82e236cf0b174aa
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-12-17 10:22:54 -0400

    Add generic example 🚀

--- src/main.rs
+++ src/main.rs
@@ -67,20 +67,48 @@
 
 // Panic
 
+// fn main() {
+//     fn a() -> i32 {
+//         return 1;
+//     }
+
+//     fn b() -> ! {
+//         panic!("Error");
+//     }
+
+//     let x = a();
+
+//     if x < 5 {
+//         b();
+//     } else {
+//         println!("Hello");
+//     }
+// }
+
+// Generic
+
 fn main() {
-    fn a() -> i32 {
-        return 1;
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = get_largest(&number_list);
+
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = get_largest(&char_list);
+
+    println!("The largest char is {}", result);
+}
+
+fn get_largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
     }
 
-    fn b() -> ! {
-        panic!("Error");
-    }
-
-    let x = a();
-
-    if x < 5 {
-        b();
-    } else {
-        println!("Hello");
-    }
+    largest
 }
