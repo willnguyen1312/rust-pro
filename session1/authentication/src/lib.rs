b6b694d15b2c74643d7ddfc939ab9f78f646331f
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-04-08 13:21:23 -0300

    Welcome Option 🎠

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -7,9 +7,32 @@ pub fn read_line() -> String {
     input.trim().to_string()
 }
 
-/// The version of the login that is a simple username and password
-pub fn login_simple(username: &str, password: &str) -> bool {
-    username.to_lowercase() == "admin" && password == "password"
+#[derive(PartialEq, Debug)]
+pub enum LoginAction {
+    Granted(LoginRole),
+    Denied,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum LoginRole {
+    Admin,
+    User,
+}
+
+pub fn login(username: &str, password: &str) -> Option<LoginAction> {
+    let username = username.to_lowercase();
+
+    if username != "admin" && username != "bob" {
+        return None;
+    }
+
+    if username == "admin" && password == "password" {
+        Some(LoginAction::Granted(LoginRole::Admin))
+    } else if username == "bob" && password == "password" {
+        Some(LoginAction::Granted(LoginRole::User))
+    } else {
+        Some(LoginAction::Denied)
+    }
 }
 
 #[cfg(test)]
@@ -17,9 +40,16 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_login_simple() {
-        assert!(login_simple("admin", "password"));
-        assert!(!login_simple("admin", "wrong"));
-        assert!(!login_simple("wrong", "password"));
+    fn test_login() {
+        assert_eq!(
+            login("admin", "password"),
+            Some(LoginAction::Granted(LoginRole::Admin))
+        );
+        assert_eq!(
+            login("bob", "password"),
+            Some(LoginAction::Granted(LoginRole::User))
+        );
+        assert_eq!(login("bob", "wrong"), Some(LoginAction::Denied));
+        assert_eq!(login("wrong", "password"), None);
     }
 }
