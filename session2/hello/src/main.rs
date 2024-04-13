b94aeeae2f0166a710bd58b67f2d052e51a1cf90
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-04-13 15:45:40 -0300

    Work with results returned from thread 💞

--- session2/hello/src/main.rs
+++ session2/hello/src/main.rs
@@ -1,6 +1,16 @@
-fn hello_from_thread(i: u32) {
+fn _hello_from_thread(i: u32) {
     // This will print the thread id
     println!("Hello from thread {}", i);
+}
+
+fn do_something(i: u32) -> u32 {
+    let mut result = 1;
+
+    for _ in 0..i {
+        result *= 2;
+    }
+
+    result
 }
 
 fn main() {
@@ -9,8 +19,11 @@ fn main() {
     let mut threads = Vec::new();
 
     for i in 0..5 {
-        threads.push(std::thread::spawn(move || hello_from_thread(i)));
+        threads.push(std::thread::spawn(move || do_something(i)));
     }
 
-    threads.into_iter().for_each(|t| t.join().unwrap());
+    threads.into_iter().for_each(|t| {
+        let result = t.join().unwrap();
+        println!("Result: {}", result);
+    });
 }
